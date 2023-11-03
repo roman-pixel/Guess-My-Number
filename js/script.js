9bcc977dc504c59b80a187cfcd41d30300988ddd
roman-pixel <romansrgvch@outlook.com>
2023-11-03 14:33:18 +0300

    finished

---
+++ js/script.js
@@ -0,0 +1,61 @@
+"use strict";
+
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let highScore = 0;
+
+const displayMessage = function (message) {
+  document.querySelector(".message").textContent = message;
+};
+
+document.querySelector(".check").addEventListener("click", function () {
+  const guess = Number(document.querySelector(".guess").value);
+
+  // no input
+  if (!guess) {
+    displayMessage("⛔️ No number!");
+
+    // win
+  } else if (guess === secretNumber) {
+    displayMessage("🎉 Correct Number!");
+    document.querySelector(".highscore").textContent =
+      highScore < score ? (highScore = score) : highScore;
+    document.querySelector(".number").textContent = secretNumber;
+
+    document.querySelector("body").style.backgroundColor = "#60b347";
+    document.querySelector(".number").style.width = "30rem";
+    document.querySelector(".check").setAttribute("disabled", "");
+
+    // guess is wrong
+  } else if (guess !== secretNumber) {
+    if (score > 1) {
+      displayMessage(guess > secretNumber ? "📈 Too high!" : "📉 Too low!");
+      score--;
+      document.querySelector(".score").textContent = score;
+    } else {
+      displayMessage("😢 You lost the game!");
+      score = 0;
+      document.querySelector(".score").textContent = score;
+      document.querySelector(".number").textContent = secretNumber;
+
+      document.querySelector("body").style.backgroundColor = "#c33838";
+      document.querySelector(".number").style.width = "30rem";
+      document.querySelector(".check").setAttribute("disabled", "");
+    }
+  }
+});
+
+document.querySelector(".again").addEventListener("click", function () {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+  displayMessage("Start guessing...");
+  document.querySelector(".score").textContent = score;
+  document.querySelector(".number").textContent = "?";
+  document.querySelector(".guess").value = "";
+
+  document.querySelector(".check").removeAttribute("disabled");
+
+  document.querySelector(".number").style.width = "15rem";
+  document.querySelector("body").style.backgroundColor = "#222";
+});
