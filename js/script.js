fd8c034b09c364b54808c4947eb0136dc4f30420
roman-pixel <romansrgvch@outlook.com>
2023-11-04 22:32:17 +0300

    bug fix

--- js/script.js
+++ js/script.js
@@ -23,7 +23,7 @@ document.querySelector(".check").addEventListener("click", function () {
     document.querySelector(".number").textContent = secretNumber;
 
     document.querySelector("body").style.backgroundColor = "#60b347";
-    document.querySelector(".number").style.width = "30rem";
+    document.querySelector(".number").style.width = "25rem";
     document.querySelector(".check").setAttribute("disabled", "");
 
     // guess is wrong
